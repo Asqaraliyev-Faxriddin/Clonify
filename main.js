17ecc437092b8f1b362c8c4cc7a39057b736b88c
Asqaraliyev-Faxriddin <asqaraliyevfaxriddin2011@gmail.com>
2025-08-09 16:44:35 +0500

    sor

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', ()=> {
+    let header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
